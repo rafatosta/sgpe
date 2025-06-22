e7af60522ae6fb8565f7a8d870fd929d93c4bae9
Rafael Tosta <rafa.ecomp@gmail.com>
2025-06-22 09:43:45 -0300

    refactor: alterar estilo da paginação

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -12,12 +12,12 @@ function Pagination({ currentPage, totalRecords, onPageChange }: PaginationProps
     const totalPages = Math.ceil(totalRecords / pageSize)
 
     return (
-        <div className="flex overflow-x-auto sm:justify-center p-2">
+        <div className="flex overflow-x-auto p-2">
             {totalRecords > 0 ? (
-                <div className="flex justify-between items-center w-full z-10">
-                    <p className="text-sm text-gray-500">Total de {totalRecords} registros</p>
+                <div className="flex justify-end items-center w-full gap-2">
+                    <p className="text-sm text-gray-500 pt-2">Página {currentPage} de {totalPages}</p>
                     <PaginationFlowbite
-                        layout="pagination"
+                        layout="navigation"
                         currentPage={currentPage}
                         totalPages={totalPages}
                         onPageChange={onPageChange}
