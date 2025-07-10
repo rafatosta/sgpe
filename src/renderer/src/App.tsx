29d403a5ccb0e7512213a2c4129575e16d272962
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-10 08:25:15 -0300

    refactor: mover componente Background para App.tsx

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,18 +3,20 @@ import { HashRouter as Router, Route, Routes } from 'react-router'
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+import Background from "@renderer/components/Background";
 
 function App(): React.JSX.Element {
 
   return (
-   
+    <div className='bg-white dark:bg-gray-900 min-h-screen'>
+      <Background />
       <Router>
         <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
       </Router>
-    
+    </div>
   )
 }
 
